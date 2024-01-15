b5ba86421b7e6865d12891a33be869fb766b6be3
Raiyan <tajkierhaque@gmail.com>
2024-01-15 16:07:02 +0600

    appointments filtering done

--- server/appointment/views.py
+++ server/appointment/views.py
@@ -6,3 +6,12 @@ from . import serializers
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = models.Appointment.objects.all()
     serializer_class  = serializers.AppointSerializers
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        patient_id = self.request.query_params.get("patient_id")
+
+        if patient_id:
+            queryset = queryset.filter(patient_id=patient_id)
+
+        return queryset
